dd39642ef2a036fd19755cbae1d092cc156915e0
nyi <35267981+corleonyis@users.noreply.github.com>
2023-01-08 15:06:35 +0000

    タスク詳細表示機能を追加
    * showアクションの追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -34,6 +34,10 @@ class TasksController < ApplicationController
 		end
 	end
 
+	def show
+		@task = Task.find(params[:id])
+	end
+
 	private
 		def task_params
 			params.require(:task).permit(:title)
