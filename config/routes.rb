fcc5147d082d963d2b2f96f03ce176ebeaf7a232
nyi <35267981+corleonyis@users.noreply.github.com>
2023-01-17 13:45:26 +0000

    ユーザー登録・解除の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root "tasks#index"
   resources :tasks
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  get '/users/:id', to: 'users#show', as: 'profile'
+  delete '/users/:id', to: 'users#destroy', as: 'unsubscribe'
 end
