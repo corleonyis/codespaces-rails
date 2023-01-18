e4261ea316c2d8d7013e9326350299b798845040
nyi <35267981+corleonyis@users.noreply.github.com>
2023-01-18 14:05:54 +0000

    リファクタリング

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,30 @@
 class UsersController < ApplicationController
-    def new
-        @user = User.new
-      end
+  before_action :require_login, only: [:show, :destroy]
+
+  def new
+    @user = User.new
+  end
     
-      def create
-        @user = User.new(user_params)
-        if @user.save
-          redirect_to profile_path(@user)
-        else
-          render 'new'
-        end
-      end
-    
-      def show
-        @user = User.find(params[:id])
-      end
-    
-      def destroy
-        user = User.find(params[:id])
-        user.destroy
-        redirect_to signup_path
-      end
-    
-      private
-        def user_params
-          params.require(:user).permit(:name, :email, :password, :password_confirmation)
-        end
+  def create
+    user = User.new(user_params)
+    if user.save
+      log_in(user)
+      redirect_to profile_path(user)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+    @current_user.destroy
+    redirect_to signup_path
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
