2d2bf697860c97218eb43cc517cd0f925959cef0
nyi <35267981+corleonyis@users.noreply.github.com>
2023-01-22 12:05:39 +0000

    タスクとユーザーの関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
     validates :password, presence: true, length: { minimum: 6 }
     has_secure_password
+    has_many :tasks, foreign_key: :user_id, dependent: :destroy
 end
